5d6a0578d82dc26e91d9f1bd49a834a07d32b3a2
tokubass <tokubass@cpan.org>
2017-11-30 22:32:42 +0900

    add handle_expr function

--- lib/eex/x/plain_text_engine.ex
+++ lib/eex/x/plain_text_engine.ex
@@ -2,17 +2,38 @@ defmodule EEx.X.PlainTextEngine do
   @moduledoc """
   Documentation for EEx.X.PlainTextEngine.
   """
+  use EEx.Engine
 
   @doc """
-  Hello world.
 
   ## Examples
-
-      iex> EEx.X.PlainTextEngine.hello
-      :world
+    iex> EEx.eval_string(
+    ...> "<%= role %>, <%= port %>, <%= text %>",
+    ...> [ role: :api, port: 9000, text: "sample" ],
+    ...> engine: EEx.X.PlainTextEngine
+    ...> )
+    ":api, 9000, \"sample\""
 
   """
-  def hello do
-    :world
+  def handle_expr(buffer, "=", expr) do
+    quote do
+      require unquote(__MODULE__)
+      if unquote(__MODULE__).is_charlist?(unquote(expr)) do
+        raise "not allow charlist in config file"
+      end
+
+      unquote(buffer) <> inspect(unquote(expr))
+    end
   end
+
+  def handle_expr(buffer, mark, expr) do
+    super(buffer, mark, expr)
+  end
+
+  defmacro is_charlist?(x) do
+    quote do
+      is_list(unquote(x)) && inspect(unquote(x)) =~ ~r/\'/
+    end
+  end
+
 end
--- test/eex_x_plain_text_engine_test.exs
+++ test/eex_x_plain_text_engine_test.exs
@@ -1,8 +1,20 @@
 defmodule EEx.X.PlainTextEngineTest do
   use ExUnit.Case
-  doctest EEx.X.PlainTextEngine
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "basic" do
+    str =  EEx.eval_string("""
+config :app, :role, <%= role %>
+config :app, :zzz,
+  port: <%= port %>,
+  text: <%= text %>
+""", [ role: :api, port: 9000, text: "sample" ], engine: EEx.X.PlainTextEngine)
+
+    assert """
+config :app, :role, :api
+config :app, :zzz,
+  port: 9000,
+  text: "sample"
+""" == str
+
   end
 end
