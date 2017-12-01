a12eaa941424dfcf05573ba96dd6c45101e8fa31
tokubass <tokubass@cpan.org>
2017-12-01 15:48:52 +0900

    export to_plain function

--- lib/eex/x/plain_text_engine.ex
+++ lib/eex/x/plain_text_engine.ex
@@ -2,38 +2,76 @@ defmodule EEx.X.PlainTextEngine do
   @moduledoc """
   Documentation for EEx.X.PlainTextEngine.
   """
+
+  defmacro __using__(_opts) do
+    quote do
+      import unquote(__MODULE__), only: [to_plain: 1]
+    end
+  end
+
   use EEx.Engine
+
+  defstruct mark: true, value: nil
 
   @doc """
 
   ## Examples
     iex> EEx.eval_string(
-    ...> "<%= role %>, <%= port %>, <%= text %>",
-    ...> [ role: :api, port: 9000, text: "sample" ],
+    ...> "<%= atom %>",
+    ...> [ atom: to_plain(:api) ],
     ...> engine: EEx.X.PlainTextEngine
     ...> )
-    ":api, 9000, \"sample\""
+    ":api"
 
   """
-  def handle_expr(buffer, "=", expr) do
+
+  def to_plain(x) do
+    %__MODULE__{value: x}
+  end
+  
+  def is_my_struct?(x) do
+    is_map(x) && Map.has_key?(x, :__struct__) && x.__struct__ == EEx.X.PlainTextEngine
+  end
+
+  def handle_expr(buf, "=", expr) do
     quote do
       require unquote(__MODULE__)
-      if unquote(__MODULE__).is_charlist?(unquote(expr)) do
-        raise "not allow charlist in config file"
+    
+      u_buf = unquote(buf)
+      u_expr   = unquote(expr)
+
+      expr_string =  if unquote(__MODULE__).is_my_struct?(unquote(expr)) do
+         val =  u_expr.value
+         case val do
+           _ when is_list(val) 
+             ->
+             if Enum.all?(val, fn(x) -> is_integer(x) end) do
+               unquote(__MODULE__).normalize_for_print(unquote(expr).value)
+               |> inspect
+               |> String.replace_prefix(~s("), "")
+               |> String.replace_suffix(~s("), "")
+             else
+               inspect(val)   
+             end
+           _
+             -> inspect(val)
+         end
+      else
+        String.Chars.to_string(u_expr)
       end
 
-      unquote(buffer) <> inspect(unquote(expr))
+       u_buf <> expr_string
     end
   end
 
-  def handle_expr(buffer, mark, expr) do
-    super(buffer, mark, expr)
+  def handle_expr(buf, mark, expr) do
+    super(buf, mark, expr)
   end
 
-  defmacro is_charlist?(x) do
+  defmacro normalize_for_print(list) do
     quote do
-      is_list(unquote(x)) && inspect(unquote(x)) =~ ~r/\'/
+      ([0] ++ unquote(list)) |> inspect |> String.replace_prefix("[0, ", "[")
     end
   end
-
+  
 end
--- test/eex_x_plain_text_engine_test.exs
+++ test/eex_x_plain_text_engine_test.exs
@@ -1,20 +1,88 @@
 defmodule EEx.X.PlainTextEngineTest do
   use ExUnit.Case
+  use EEx.X.PlainTextEngine
 
-  test "basic" do
-    str =  EEx.eval_string("""
-config :app, :role, <%= role %>
-config :app, :zzz,
-  port: <%= port %>,
-  text: <%= text %>
-""", [ role: :api, port: 9000, text: "sample" ], engine: EEx.X.PlainTextEngine)
-
-    assert """
-config :app, :role, :api
-config :app, :zzz,
-  port: 9000,
-  text: "sample"
-""" == str
-
+  test "plain atom" do
+    str =  EEx.eval_string(
+      "<%= atom %>",
+      [atom: to_plain(:api)],
+      engine: EEx.X.PlainTextEngine
+    )
+    assert ":api" == str
   end
+
+  test "string" do
+    str =  EEx.eval_string(
+      "<%= string %>",
+      [string: "test"],
+      engine: EEx.X.PlainTextEngine
+    )
+    assert "test" == str
+  end
+
+  test "plain string" do
+    str =  EEx.eval_string(
+      "<%= string %>",
+      [string: to_plain("test")],
+      engine: EEx.X.PlainTextEngine
+    )
+    assert ~s("test") == str
+  end
+
+  test "char" do
+    str =  EEx.eval_string(
+      "<%= char %>",
+      [char: 'test'],
+      engine: EEx.X.PlainTextEngine
+    )
+    assert "test" == str
+  end
+
+  test "nor support char" do
+    str =  EEx.eval_string(
+      "<%= char %>",
+      [char: to_plain('test')],
+      engine: EEx.X.PlainTextEngine
+    )
+    assert "[116, 101, 115, 116]" == str
+  end
+
+  test "plain list" do
+    str =  EEx.eval_string(
+      "<%= list %>",
+      [list: to_plain([a: 1])],
+      engine: EEx.X.PlainTextEngine
+    )
+    assert "[a: 1]" == str
+  end
+
+  test "plain map" do
+    str =  EEx.eval_string(
+      "<%= map %>",
+      [map: to_plain(%{a: 1})],
+      engine: EEx.X.PlainTextEngine
+    )
+    assert "%{a: 1}" == str
+  end
+ 
+
+  defstruct name: "aaa", tmp: :tmp
+  test "plain struct" do
+    str =  EEx.eval_string(
+      "<%= struct  %>",
+      [struct: to_plain(%__MODULE__{})],
+      engine: EEx.X.PlainTextEngine
+    )
+    assert "%EEx.X.PlainTextEngineTest{name: \"aaa\", tmp: :tmp}" == str
+  end
+
+  test "mistakable_list" do
+    str =  EEx.eval_string(
+      "<%= list %>",
+      [list: "[10, 100]"],
+      engine: EEx.X.PlainTextEngine
+    )
+    assert "[10, 100]" == str
+  end
+  
 end
